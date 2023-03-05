9e7bba6058a43f89e8fbb660a22cf447ccee8963
willbillionquant <willbillion2017@gmail.com>
2023-03-06 02:47:15 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ codepath_bsoption = os.path.dirname(os.path.abspath(__file__))
 import sys
 sys.path.append('..')
 
-def getfinalopprice(optype, strike, price):
+def getFinalOpPrice(opType, strike, price):
     """Obtain the option price at expiry."""
-    assert optype in ['C', 'P'], AttributeError('Option type has to be "C" or "P"!')
-    return (max(price - strike, 0), max(strike - price, 0))[(optype == 'P')]
+    assert opType in ['C', 'P'], AttributeError('Option type has to be "C" or "P"!')
+    return (max(price - strike, 0), max(strike - price, 0))[(opType == 'P')]
