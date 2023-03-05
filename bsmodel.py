6d00520cd06e00e9fc4b356657fcb5e0df09b5d3
willbillionquant <willbillion2017@gmail.com>
2023-03-06 03:05:43 +0800

    Update bsmodel.py

--- bsmodel.py
+++ bsmodel.py
@@ -21,98 +21,100 @@ class BSModel():
         self.T = T / 365  # Number of days to expiry
         self.sig = sig  # IV ( 50% = 0.5)
         self.rf = rf  # risk-free rate
-        self.d_1 = self.getzscore()[0]
-        self.d_2 = self.getzscore()[1]
+        self.d1 = self.getZscore()[0]
+        self.d2 = self.getZscore()[1]
         self.cPrice = self.getOpPrice('C')
         self.pPrice = self.getOpPrice('P')
         self.cDelta = self.getDelta('C')
         self.pDelta = self.getDelta('P')
         self.cTheta = self.getTheta('C')
         self.pTheta = self.getTheta('P')
-        self.Vega = round(self.S * norm.pdf(self.d_1) * (self.T ** 0.5) / 100, 6)
-        self.Gamma = round(norm.pdf(self.d_1) / (self.S * self.sig * (self.T ** 0.5)), 9)
+        self.Vega = round(self.S * norm.pdf(self.d1) * (self.T ** 0.5) / 100, 6)
+        self.Gamma = round(norm.pdf(self.d1) / (self.S * self.sig * (self.T ** 0.5)), 9)
 
-    def getzscore(self):
+    def getZscore(self):
         """Compute two essential z-scores for option pricing."""
-        d_1 = (np.log(self.S / self.K) + self.T * (self.rf + 0.5 * (self.sig ** 2))) / (self.sig * (self.T ** 0.5))
-        d_2 = d_1 - self.sig * (self.T ** 0.5)
-        return d_1, d_2
+        d1 = (np.log(self.S / self.K) + self.T * (self.rf + 0.5 * (self.sig ** 2))) / (self.sig * (self.T ** 0.5))
+        d2 = d1 - self.sig * (self.T ** 0.5)
+        return d1, d2
 
-    def getOpPrice(self, optype):
+    def getOpPrice(self, opType):
         """Compute option price by Black-Scholes Model."""
-        assert optype in ['C', 'P'], AttributeError('Must be call or put!')
-        if optype == 'C':
-            opPrice = self.S * norm.cdf(self.d_1, 0, 1) - self.K * np.exp(- self.rf * self.T) * norm.cdf(self.d_2, 0, 1)
+        assert opType in ['C', 'P'], AttributeError('Must be call or put!')
+        if opType == 'C':
+            opPrice = self.S * norm.cdf(self.d1, 0, 1) - self.K * np.exp(- self.rf * self.T) * norm.cdf(self.d2, 0, 1)
         else:
-            opPrice = self.K * np.exp(- self.rf * self.T) * norm.cdf(-self.d_2, 0, 1) - self.S * norm.cdf(-self.d_1, 0, 1)
+            opPrice = self.K * np.exp(- self.rf * self.T) * norm.cdf(-self.d2, 0, 1) - self.S * norm.cdf(-self.d1, 0, 1)
 
         return round(opPrice, 4)
 
-    def getDelta(self, optype):
+    def getDelta(self, opType):
         """Return call or put delta = inceremental change per unit increment in underlying."""
-        assert optype in ['C', 'P'], AttributeError('Must be call or put!')
-        if optype == 'C':
-            delta = norm.cdf(self.d_1)
+        assert opType in ['C', 'P'], AttributeError('Must be call or put!')
+        if opType == 'C':
+            delta = norm.cdf(self.d1)
         else:
-            delta = norm.cdf(self.d_1) - 1
+            delta = norm.cdf(self.d1) - 1
         return round(delta, 6)
 
-    def getTheta(self, optype):
+    def getTheta(self, opType):
         """Return call or put theta = time value per day."""
-        assert optype in ['C', 'P'], AttributeError('Must be call or put!')
-        if optype == 'C':
-            t_1 = - self.S * norm.pdf(self.d_1) * self.sig / (2 * (self.T) ** 0.5)
-            t_2 = self.rf * self.K * np.exp(- self.rf * self.T) * norm.cdf(self.d_2)
+        assert opType in ['C', 'P'], AttributeError('Must be call or put!')
+        if opType == 'C':
+            t1 = - self.S * norm.pdf(self.d1) * self.sig / (2 * (self.T) ** 0.5)
+            t2 = self.rf * self.K * np.exp(- self.rf * self.T) * norm.cdf(self.d2)
         else:
-            t_1 = - self.S * norm.pdf(self.d_1) * self.sig / (2 * (self.T) ** 0.5)
-            t_2 = self.rf * self.K * np.exp(- self.rf * self.T) * norm.cdf(- self.d_2)
-        return round((t_1 + t_2) / 365, 6)
+            t1 = - self.S * norm.pdf(self.d1) * self.sig / (2 * (self.T) ** 0.5)
+            t2 = self.rf * self.K * np.exp(- self.rf * self.T) * norm.cdf(- self.d2)
+        return round((t1 + t2) / 365, 6)
 
-    def getpayoff(self, preexpiry=False, numday=(7, 28, 56), opside='LONG'):
-        """Obtain payoff diagram at expiry and (if `preexpiry` enabled) payoff of each given days before expiry."""
-        assert opside in ['LONG', 'SHORT'], AttributeError('opside must be LONG or SHORT!')
+    def getpayoff(self, numday=(7, 28, 56), Long=True, preExpiry=False):
+        """Obtain payoff diagram at expiry and (if `preExpiry` enabled) payoff of each given days before expiry."""
         halfplus = lambda x: x if x > 0 else 0
         lowb = self.K * (1 - self.sig / 2)
         upb = self.K * (1 + self.sig / 2)
-        pricearr = np.linspace(lowb, upb, 100)
+        priceArray = np.linspace(lowb, upb, 100)
         # Payoff Dataframe: call & put at expiry
         dfprice = pd.DataFrame(columns=['spot', 'expC', 'expP'])
-        dfprice['spot'] = pricearr
-        dfprice['expC'] = (dfprice['spot'] - self.K).apply(halfplus)
-        dfprice['expP'] = (self.K - dfprice['spot']).apply(halfplus)
-        if opside == 'SHORT':
-            dfprice['expC'] *= -1
-            dfprice['expP'] *= -1
+        dfprice['spot'] = priceArray
+        dfprice['expC'] = (dfprice['spot'] - self.K).apply(halfplus) * (1 if Long else -1)
+        dfprice['expP'] = (self.K - dfprice['spot']).apply(halfplus) * (1 if Long else -1)
         # Add payoff plots
         fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.05,
-                            row_heights=[0.5, 0.5], specs=[[{"type": "scatter"}]] * 2,
-                            subplot_titles=("Call", "Put"))
+                            row_heights=[0.5, 0.5], specs=[[{"type": "scatter"}]] * 2, subplot_titles=("Call", "Put"))
+
         fig.add_trace(go.Scatter(x=dfprice['spot'], y=dfprice['expC'],
                                  mode="lines", name="Call-Exp", line_color='#43b117'), row=1, col=1)
+
         fig.add_trace(go.Scatter(x=dfprice['spot'], y=dfprice['expP'],
                                  mode="lines", name="Put-Exp", line_color='#1756b1'), row=2, col=1)
-        if preexpiry:
+
+        if preExpiry:
             for day in numday:
                 dfprice[f'{day}dayC'] = round(dfprice['spot'].apply(lambda x: BSModel(x, self.K, day, self.sig).cPrice), 2)
                 dfprice[f'{day}dayP'] = round(dfprice['spot'].apply(lambda x: BSModel(x, self.K, day, self.sig).pPrice), 2)
-                if opside == 'SHORT':
+
+                if not Long:
                     dfprice[f'{day}dayC'] *= -1
                     dfprice[f'{day}dayP'] *= -1
+
                 fig.add_trace(go.Scatter(x=dfprice['spot'], y=dfprice[f'{day}dayC'],
                                          mode="markers", name=f"Call-{day}D", line_color='#d516cc'), row=1, col=1)
+
                 fig.add_trace(go.Scatter(x=dfprice['spot'], y=dfprice[f'{day}dayP'],
                                          mode="markers", name=f"Put-{day}D", line_color='#d51653'), row=2, col=1)
 
-        fig.update_layout(height=800, showlegend=False, title_text=f'{opside}-side Payoff curve', title_x=0.5)
+        side = 'LONG' if Long else 'SHORT'
+        fig.update_layout(height=800, showlegend=False, title_text=f'{side}-side Payoff curve', title_x=0.5)
         fig.show()
 
-def getivbisect(S, K, T, P, optype, rf=0, lowb=0, upb=400.0, maxstep=20, pcterr=0.001):
+def getivbisect(S, K, T, P, opType, rf=0, lowb=0, upb=400.0, maxstep=20, pcterr=0.001):
     """Obtain an estimate of IV by bisection method."""
     BSstart = BSModel(S, K, T, lowb / 100, rf)  # Lower estimate of original option price
     BSend = BSModel(S, K, T, upb / 100, rf)  # Upper estimate of original option price
-    assert optype in ['C', 'P'], AttributeError('Must be call or put!')
-    lowprice = BSstart.getOpPrice(optype)
-    upPrice = BSend.getOpPrice(optype)
+    assert opType in ['C', 'P'], AttributeError('Must be call or put!')
+    lowprice = BSstart.getOpPrice(opType)
+    upPrice = BSend.getOpPrice(opType)
     assert ((P >= lowprice) and (P <= upPrice)), AttributeError('IV should be between lower & upper bounds!')
 
     if (T == 0) or (P == abs(S - K)):
@@ -129,7 +131,7 @@ def getivbisect(S, K, T, P, optype, rf=0, lowb=0, upb=400.0, maxstep=20, pcterr=
                 break
             else:  # Take mid-point of current interval and cut interval in half
                 sig = (lowb + upb) / 2
-                newprice = BSModel(S, K, T, sig / 100, rf).getOpPrice(optype)
+                newprice = BSModel(S, K, T, sig / 100, rf).getOpPrice(opType)
                 if newprice > P:
                     upb = sig
                     upPrice = newprice
